fd304c12667b06114424f0edc8080115487d0778
Mahib09 <Mahib.Ranamagar@triosstudent.com>
2024-03-24 02:05:57 -0400

    counter function added

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ function addTextField() {
             <button class="containerbtn deletebtn">&#10540;</button>`;
       contentContainer.innerHTML = newContent;
       content.insertBefore(contentContainer, button);
+
+      //counter
     });
   });
 }
@@ -22,6 +24,14 @@ function addTextField() {
 document.addEventListener("click", function (event) {
   if (event.target && event.target.classList.contains("deletebtn")) {
     const container = event.target.closest(".contentContainer");
+    const NearestTaskcomponent = container.closest(".taskComponent");
+    const counterbtn = NearestTaskcomponent.querySelector(".counter");
+    const allContent = NearestTaskcomponent.querySelectorAll(
+      ".contentContainer textarea"
+    );
+    var counterValue = Number(allContent.length);
+    var newCount = counterValue - 1;
+    counterbtn.innerHTML = newCount;
     container.remove();
   }
 });
@@ -32,6 +42,12 @@ document.addEventListener("click", function (event) {
     function moveitem() {
       const btn = event.target;
       const content = btn.closest(".contentContainer");
+      const currentlocation = btn.closest(".taskComponent");
+      const currentcounter = currentlocation.querySelector(".counter");
+      var currentvalue = Number(currentcounter.innerHTML);
+      var newcurrentvalue = currentvalue - 1;
+      currentcounter.innerHTML = newcurrentvalue;
+
       moveContainer = function () {
         const parentComponent = btn.closest(".taskComponent");
         if (parentComponent.classList.contains("todo")) {
@@ -41,8 +57,15 @@ document.addEventListener("click", function (event) {
         }
       };
       const movelocation = moveContainer();
+      const movecounterbtn = movelocation.querySelector(".counter");
       const contentLocation = movelocation.querySelector(".content");
+      const moveallContent = movelocation.querySelectorAll(
+        ".contentContainer textarea"
+      );
+      var movecounterValue = Number(moveallContent.length);
+      var movenewCount = movecounterValue + 1;
       const nearButton = movelocation.querySelector(".content .new-taskbutton");
+      movecounterbtn.innerHTML = movenewCount;
       contentLocation.insertBefore(content, nearButton);
     }
     moveitem();
@@ -68,8 +91,30 @@ document.addEventListener("click", function (event) {
       const newContent = `<textarea name="" id="" cols="30" rows="10" placeholder="Type a Name"></textarea> <button class="containerbtn arrowbtn">-></button>
             <button class="containerbtn deletebtn">X</button>`;
       contentContainer.innerHTML = newContent;
-      content.insertBefore(contentContainer, nearBtn); // Corrected variable name
+      content.insertBefore(contentContainer, nearBtn);
+      //counter
+      const NearestTaskcomponent = contentContainer.closest(".taskComponent");
+      const counterbtn = NearestTaskcomponent.querySelector(".counter");
+      const allContent = NearestTaskcomponent.querySelectorAll(
+        ".contentContainer textarea"
+      );
+      var counterValue = Number(allContent.length);
+      var newCount = counterValue;
+      counterbtn.innerHTML = newCount;
     }
     addTextField();
   }
 });
+// window.addEventListener("load", (e) => {
+//   counter();
+// });
+function counter() {
+  const counterButton = document.querySelectorAll("counter");
+  counterButton.forEach((e) => {
+    const main = e.closest(".taskComponent");
+    console.log(main);
+    const allContent = main.querySelectorAll(".contentContainer");
+    var count = allContent.length;
+    e.innerHTML = count;
+  });
+}
