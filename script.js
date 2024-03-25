35eadf16cfb92a9026d2ca436708cdeef2c5ab94
Mahib09 <Mahib.Ranamagar@triosstudent.com>
2024-03-25 03:00:03 -0400

    Task Drag and Drop Function with sort made

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ document.addEventListener("click", function (event) {
       counterbtn.innerHTML = counterValue;
     }
     addTextField();
+    drag();
   }
 });
 
@@ -115,6 +116,7 @@ document.addEventListener("click", function (event) {
       counterbtn.innerHTML = counterValue;
     }
     addTextField();
+    drag();
   }
 });
 
@@ -136,8 +138,32 @@ function drag() {
   dragContainers.forEach((container) => {
     container.addEventListener("dragover", (e) => {
       e.preventDefault();
+      const afterElement = getDragAfterElement(container, e.clientY);
       const draggable = document.querySelector(".dragging");
-      container.appendChild(draggable);
+      const nearbtn = container.querySelector(".new-taskbutton");
+      if (afterElement == null) {
+        container.insertBefore(draggable, nearbtn);
+      } else {
+        container.insertBefore(draggable, afterElement);
+      }
     });
   });
+}
+
+function getDragAfterElement(container, y) {
+  const draggableElements = [
+    ...container.querySelectorAll(".contentContainer:not(.dragging)"),
+  ];
+  return draggableElements.reduce(
+    (closest, child) => {
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child };
+      } else {
+        return closest;
+      }
+    },
+    { offset: Number.NEGATIVE_INFINITY }
+  ).element;
 }
