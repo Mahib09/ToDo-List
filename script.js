dc537f3b4c2b757b14aa923c96646687b0942a54
Mahib09 <Mahib.Ranamagar@triosstudent.com>
2024-04-04 23:50:21 -0400

    select a list function created

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const listContainer = document.querySelector("[data-lists]");
-const lists = [];
 const newlistForm = document.querySelector("[data-new-list-form]");
 const newlistInput = document.querySelector("[data-new-list-input]");
-function render() {
-  clearElement(listContainer);
-  lists.forEach((list) => {
-    const listContent = document.createElement("li");
-    listContainer.dataset.listId = list.id;
-    listContent.classList.add("list");
-    listContent.innerText = list.name;
-    listContainer.appendChild(listContent);
-  });
-}
+const deleteListButton = document.querySelector("[data-delete-list-button]");
+const LOCAL_STORAGE_LIST_KEY = "tak.lists";
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "task.selectedListId";
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
+
+listContainer.addEventListener("click", (e) => {
+  console.log("click woeking");
+  if (e.target.tagName.toLowerCase() === "li") {
+    selectedListId = e.target.dataset.listId;
+    saveandrender();
+  }
+});
+deleteListButton.addEventListener("click", (e) => {});
 newlistForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const listname = newlistInput.value;
@@ -21,7 +24,7 @@ newlistForm.addEventListener("submit", (e) => {
   const list = createList(listname);
   newlistInput.value = "";
   lists.push(list);
-  render();
+  saveandrender();
 });
 function clearElement(element) {
   while (element.firstChild) {
@@ -30,4 +33,24 @@ function clearElement(element) {
 }
 function createList(name) {
   return { id: Date.now().toString(), name: name, task: [] };
+}
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+}
+function saveandrender() {
+  save();
+  render();
+}
+function render() {
+  clearElement(listContainer);
+  lists.forEach((list) => {
+    const listContent = document.createElement("li");
+    listContent.dataset.listId = list.id;
+    listContent.classList.add("list");
+    listContent.innerText = list.name;
+    if (list.id === selectedListId) {
+      listContent.classList.add("selectedList");
+    }
+    listContainer.appendChild(listContent);
+  });
 }
